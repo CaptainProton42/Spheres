d874d4ee396ac649626254a24016806c3be7451d
John Wigg <john.wigg@uni-jena.de>
2019-02-20 23:56:50 +0100

    fixed model

--- spheres.js
+++ spheres.js
@@ -96,6 +96,8 @@ var loader = new THREE.GLTFLoader();
 loader.load( 'models/observer.glb', function ( gltf ) {
 
     var mesh =  gltf.scene.children[0]
+    var material = new THREE.MeshBasicMaterial( {color: 0x666666 } );
+    mesh.material = material
     scene.add(mesh)
 
 }, undefined, function ( error ) {
