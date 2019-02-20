675a029f764d88b421acc484e6063bbb4d22ced9
John Wigg <john.wigg@uni-jena.de>
2019-02-20 23:05:29 +0100

    initial commit

---
+++ horizontal.js
@@ -0,0 +1,150 @@
+var scene = new THREE.Scene();
+
+// Comnfigure camera.
+var frustumSize = 5
+var aspect = window.innerWidth / window.innerHeight;
+var camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0, 1000 );
+camera.position.x = -2;
+camera.position.z = -2;
+camera.position.y = 1;
+
+// Configure the WebGL renderer.
+var renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
+document.body.appendChild( renderer.domElement );
+
+// Add controls. We want only rotation.
+controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.enableDamping = true;
+controls.rotateSpeed = 0.25;
+
+// Line styles. Colors taken from https://learnui.design/tools/data-color-picker.html
+lineStyle1 = new LineStyle3D(0.005, 0x003f5c);
+lineStyle2 = new LineStyle3D(0.005, 0xffa600);
+lineStyle3 = new LineStyle3D(0.005, 0xbc5090);
+lineStyleLight = new LineStyle3D(0.005, 0x666666);
+lineStyleArrow = new LineStyle3D(0.005, 0xff6361, true);
+
+// 3D curves and lines.
+horizon = new Circle3D(1, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0));
+meridian = new Circle3D(1, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(1.0, 0.0, 0.0));
+ZN = new Line3D(new THREE.Vector3(0.0, -1.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0));
+NS = new Line3D(new THREE.Vector3(0.0, 0.0, 1.0), new THREE.Vector3(0.0, 0.0, -1.0));
+WE = new Line3D(new THREE.Vector3(-1.0, 0.0, 0.0), new THREE.Vector3(1.0, 0.0, 0.0));
+
+// Set styles.
+horizon.setStyle(lineStyle1);
+meridian.setStyle(lineStyle2);
+ZN.setStyle(lineStyleLight);
+NS.setStyle(lineStyleLight);
+WE.setStyle(lineStyleLight);
+
+// 3D surfaces.
+horizonDisc = new DiscSurface3D(1, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0), 0x003f5c);
+meridianDisc = new DiscSurface3D(1, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(1.0, 0.0, 0.0), 0xffa600);
+
+// Markers.
+S = new DirectionMarker(new THREE.Vector3(0.0, 0.0, 1.1), new THREE.Vector3(0.0, 0.0, 1.0), 0.1, 0x666666);
+N = new FlatDot(new THREE.Vector3(0.0, 0.0, -1.1), new THREE.Vector3(0.0, 1.0, 0.0), 0.05, 0x666666);
+W = new FlatDot(new THREE.Vector3(1.1, 0.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0), 0.05, 0x666666);
+E = new FlatDot(new THREE.Vector3(-1.1, 0.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0), 0.05, 0x666666);
+SLabel = new FlatText(new THREE.Vector3(0.0, 0.0, 1.3), new THREE.Vector3(0.0, 1.0, 0.0), Math.PI, 0.1, "S", 0x666666);
+NLabel = new FlatText(new THREE.Vector3(0.0, 0.0, -1.3), new THREE.Vector3(0.0, 1.0, 0.0), 0.0, 0.1, "N", 0x666666);
+WLabel = new FlatText(new THREE.Vector3(-1.3, 0.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0), Math.PI/2, 0.1, "W", 0x666666);
+ELabel = new FlatText(new THREE.Vector3(1.3, 0.0, 0.0), new THREE.Vector3(0.0, 1.0, 0.0), -Math.PI/2, 0.1, "E", 0x666666);
+zenith = new Dot3D(new THREE.Vector3(0.0, 1.0, 0.0), 0.05, 0x666666 );
+nadir = new Dot3D(new THREE.Vector3(0.0, -1.0, 0.0), 0.05, 0x666666 );
+zenithLabel = new FlatText(new THREE.Vector3(0.0, 1.2, 0.0), new THREE.Vector3(0.0, 0.0, -1.0), 0.0, 0.1, "Zenith", 0x666666);
+nadirLabel = new FlatText(new THREE.Vector3(0.0, -1.2, 0.0), new THREE.Vector3(0.0, 0.0, -1.0), 0.0, 0.1, "Nadir", 0x666666);
+
+// Add meshes to scene.
+scene.add(horizon.getMesh());
+scene.add(meridian.getMesh());
+scene.add(ZN.getMesh());
+scene.add(NS.getMesh());
+scene.add(WE.getMesh());
+scene.add(horizonDisc.getMesh());
+scene.add(meridianDisc.getMesh());
+
+
+// TODO: tydiing up everything below here
+S = new DirectionMarker(new THREE.Vector3(0.0, 0.0, 1.1), new THREE.Vector3(0.0, 0.0, 1.0), 0.1, 0x666666);
+
+arc = new Arc3D(1, 0.0, 1.0, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(0.0, -Math.PI, Math.PI/2));
+arc.setStyle(lineStyleArrow)
+arc_surface = new ArcSurface3D(1, 0.0, 1.0, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(0.0, 0.0, 0.0), 0xA569BD);
+
+starphi =0.0;
+starpos = new THREE.Spherical(1.0, 1.0, 0.0);
+star = new Dot3D(new THREE.Vector3(0.0, 0.0,0.0), 0.03, 0xA569BD);
+
+scene.add(arc.mesh)
+scene.add(star.mesh)
+scene.add(S.mesh)
+scene.add(zenith.mesh)
+scene.add(W.mesh)
+scene.add(E.mesh)
+scene.add(S.mesh)
+scene.add(N.mesh)
+scene.add(nadir.mesh)
+
+var loader = new THREE.GLTFLoader();
+
+loader.load( 'models/observer.gltf', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    controls.update();
+
+    scene.add(SLabel.mesh) // hacky
+    scene.add(NLabel.mesh)
+    scene.add(ELabel.mesh)
+    scene.add(WLabel.mesh)
+    scene.add(zenithLabel.mesh);
+    scene.add(nadirLabel.mesh)
+
+    
+    starpos.theta -= 0.01;
+
+    var euclidian = new THREE.Vector3();
+    euclidian.setFromSpherical(starpos);
+    euclidian.applyAxisAngle(new THREE.Vector3(-1.0, 0.0, 0.0), 0.5);
+
+    star.mesh.position.x = euclidian.x;
+    star.mesh.position.y = euclidian.y;
+    star.mesh.position.z = euclidian.z;
+
+    var starpos_kk = new THREE.Spherical(0.0, 0.0, 0.0);
+    starpos_kk.setFromCartesianCoords(euclidian.x,euclidian.y, euclidian.z);
+
+    var arc_new = new Arc3D(1, 0.0, Math.PI/2 - starpos_kk.phi, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(-Math.cos(starpos_kk.theta), 0.0, Math.sin(starpos_kk.theta)));
+    arc_new.setStyle(lineStyleArrow)
+    arc_surface_new = new ArcSurface3D(1, 0.0, Math.PI/2 - starpos_kk.phi, new THREE.Vector3(0.0, 0.0, 0.0), new THREE.Vector3(-Math.cos(starpos_kk.theta), 0.0, Math.sin(starpos_kk.theta)), 0xA569BD);
+
+    scene.remove(arc.mesh);
+    scene.remove(arc_surface.mesh);
+    arc.mesh = arc_new.mesh;
+    arc_surface.mesh = arc_surface_new.mesh;
+    scene.add(arc.mesh);
+    scene.add(arc_surface.mesh);
+
+
+
+
+
+    renderer.render( scene, camera );
+};
+
+animate();
---
+++ js/Path3D.js
@@ -0,0 +1,118 @@
+class LineStyle3D {
+    constructor(width, color, arrow) {
+        this.width = width;
+        this.color = color;
+        this.arrow = arrow;
+    }
+}
+
+class Path3D {
+    constructor() {
+        this.linestyle = new LineStyle3D();
+        this.curve = new THREE.Curve();
+        this.mesh = new THREE.Mesh();
+
+        this.position = new THREE.Vector3();
+        this.normal = new THREE.Vector3();
+    };
+
+    setStyle(linestyle) {
+        this.linestyle = linestyle;
+        this.updateMesh();
+    };
+
+    updateMesh() {
+        var geometry = new THREE.TubeGeometry(this.curve, 64, this.linestyle.width, 16, false);
+        // Create the line material
+        var material = new THREE.MeshBasicMaterial( {
+            color: this.linestyle.color,
+        });
+
+        if (this.linestyle.arrow) {
+            // arrow style
+            var arrowwidth = this.linestyle.width * 5;
+            var arrowlen = 2 * arrowwidth;
+
+            // create arrow geometry
+            var arrow = new THREE.ConeGeometry(arrowwidth, arrowlen, 32);
+
+            // transformation and rotation matrices for arrow placement
+            var transform = new THREE.Matrix4();
+            var rotation = new THREE.Matrix4();
+
+            // tangent at the end of the curve
+            var tangent = this.curve.getTangent(1).normalize();
+            var quaternion = new THREE.Quaternion();
+            quaternion.setFromUnitVectors(new THREE.Vector3(0.0, 1.0, 0.0), tangent);
+            rotation.makeRotationFromQuaternion(quaternion);
+
+            // place arrow tip on curve end
+            var arrow_pos = new THREE.Vector3();
+            arrow_pos = geometry.vertices[geometry.vertices.length-1];
+            arrow_pos = arrow_pos.add(tangent.multiplyScalar(-arrowlen/2));
+
+            // apply transformations and merge
+            arrow.applyMatrix( rotation );
+            transform.setPosition(arrow_pos);
+            geometry.merge(arrow, transform);
+        }
+
+        this.mesh = new THREE.Mesh( geometry, material );
+
+
+        var n1 = new THREE.Vector3(0.0, 0.0, 1.0);
+
+        var quaternion = new THREE.Quaternion(); // create one and reuse it
+
+        quaternion.setFromUnitVectors( n1, this.normal );
+
+        var matrix = new THREE.Matrix4(); // create one and reuse it
+
+        matrix.makeRotationFromQuaternion( quaternion );
+
+        this.mesh.applyMatrix( matrix );
+
+        this.mesh.position.x = this.position.x;
+        this.mesh.position.y = this.position.y;
+        this.mesh.position.z = this.position.z;
+    };
+
+    getMesh() {
+        return this.mesh;
+    }
+}
+
+class GeometryPath3D extends Path3D {
+    constructor( path ) {
+        super();
+        this.path = path;
+        this.updateMesh();
+    }
+}
+
+class Circle3D extends Path3D {
+    constructor(radius, position, normal) {
+        super();
+        this.curve = new Circle( radius );
+        this.position = position;
+        this.normal = normal;
+        this.updateMesh();
+    }
+}
+
+class Arc3D extends Path3D {
+    constructor(radius, phi1, phi2, position, normal) {
+        super();
+        this.curve = new Arc(radius, phi1, phi2); 
+        this.position = position;
+        this.normal = normal;  
+        this.updateMesh();
+    }
+}
+
+class Line3D extends Path3D {
+    constructor(x1, x2) {
+        super();
+        this.curve = new THREE.LineCurve3(x1, x2);
+    }
+}
